cc46c6af0c081ad54b3da141e399dcf318a82324
kamal Pardeep kumar <151348140+kamalkd12@users.noreply.github.com>
2024-01-10 19:29:30 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[getRandomNumber()];
+  }
+  // console.log(hexColor);
+
+  color.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+});
+
+
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn1 = document.getElementById("btn");
+const color1 = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  const randomNumber = getRandomNumber();
+  // console.log(randomNumber);
+
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
